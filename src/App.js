930536f8974da1bcdc0818ad4151e6759d99ca6d
ShoonLei <pandapanda254.pp@gmail.com>
2021-05-27 01:18:33 +0630

    header done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './pages/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path='/search' >
+            <h1>This is Search Page</h1>
+          </Route>
+          <Route path='/' >
+            <Home/>
+          
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css'
+import { Link } from 'react-router-dom'
+import AppsIcon from '@material-ui/icons/Apps'
+import { Avatar } from '@material-ui/core'
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>store</Link>
+                </div>
+                <div className='home__headerRight'>
+                    <Link to='/email'>Email</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppsIcon/>
+                    <Avatar/>
+                </div>
+            </div>
+            <div className='home__body'>
+
+            </div>
+        </div>
+    )
+}
